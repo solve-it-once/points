ad89c08c58d7bef18044b69f32e5921cf170a65c
Jingsheng Wang <skyred@insready.com>
2017-07-27 16:14:15 +0800

    Issue #2897229: Handle point save exception.

--- src/Form/PointInlineForm.php
+++ src/Form/PointInlineForm.php
@@ -18,15 +18,18 @@ class PointInlineForm extends EntityInlineForm {
     /** @var \Drupal\points\Entity\Point $entity */
     $entity = $entity_form['#entity'];
     $user_inputs = $form_state->getUserInput();
-    if (!$user_inputs) {
+    $points_inputs = $user_inputs[$entity_form['#parents'][0]];
+    // Check if user has submit a point entity data
+    if (!$points_inputs) {
       $entity_form['state'] = [
         '#type' => 'hidden',
         '#value' => $entity->getPoints()
       ];
-    } else {
-      $points = $user_inputs[$entity_form['#parents'][0]];
       // TODO: do we need to handle when mutiple Point entities are allowed?
-      $entity->set('state', $points[0]['inline_entity_form']['state']);
+      $entity->set('state', $entity->getPoints());
+    } else {
+      // TODO: do we need to handle when mutiple Point entities are allowed?
+      $entity->set('state', $points_inputs[0]['inline_entity_form']['state']);
     }
     return $entity_form;
   }
