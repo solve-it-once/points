3afad2bbf80253d196d9ededfd8f4138e51ca97a
Brad Czerniak <ao5357@gmail.com>
2023-03-22 15:06:11 -0400

    Improve conditional handling of derivative definitions for the points local task.

--- src/Plugin/Derivative/PointsLocalTask.php
+++ src/Plugin/Derivative/PointsLocalTask.php
@@ -53,16 +53,21 @@ class PointsLocalTask extends DeriverBase implements ContainerDeriverInterface {
     $derivatives = [];
     $config_entities = Drupal::entityTypeManager()->getStorage('field_config')->loadMultiple();
     foreach ($config_entities as $config_entity) {
-      $handler = explode(':', $config_entity->getSetting('handler'));
-      if ($config_entity->getType() === 'entity_reference' && $handler[1] === 'point') {
-        $entity_type_id = $config_entity->get('entity_type');
-        $field_name = substr($config_entity->get('field_name'), 6);
-        $derivatives[$entity_type_id . '.points.' . $field_name] = [
-          'route_name' => "entity.$entity_type_id.points.movement.$field_name",
-          'title' => $this->t($config_entity->getLabel()),
-          'base_route' => 'entity.' . $entity_type_id . '.canonical',
-          'weight' => 50,
-        ];
+      if (method_exists($config_entity, 'getSetting')) {
+        $setting = $config_entity->getSetting('handler');
+        if (isset($setting) && !empty($setting)) {
+          $handler = explode(':', $setting);
+          if ($config_entity->getType() === 'entity_reference' && $handler[1] === 'point') {
+            $entity_type_id = $config_entity->get('entity_type');
+            $field_name = substr($config_entity->get('field_name'), 6);
+            $derivatives[$entity_type_id . '.points.' . $field_name] = [
+              'route_name' => "entity.$entity_type_id.points.movement.$field_name",
+              'title' => $this->t($config_entity->getLabel()),
+              'base_route' => 'entity.' . $entity_type_id . '.canonical',
+              'weight' => 50,
+            ];
+          }
+        }
       }
     }
 
